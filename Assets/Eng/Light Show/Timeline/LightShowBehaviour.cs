d461371a02a7cd99513c4049100a7ab1887d331a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-11-25 14:54:53 +0200

    Add more customizability to light show shader

--- Assets/Eng/Light Show/Timeline/LightShowBehaviour.cs
+++ Assets/Eng/Light Show/Timeline/LightShowBehaviour.cs
@@ -5,10 +5,14 @@ using UnityEngine.Playables;
 [Serializable]
 public class LightShowBehaviour : PlayableBehaviour {
     [ColorUsage(true, true)] public Color Color = Color.white * 5;
+    [Tooltip("Whether or not transparency should be calculated using Screen Blending. This makes semi-transparent elements possible.")]
+    public bool UseScreenBlending = false;
+    [Range(0f, 1f)] public float GlobalTransparency = 1f;
     [Min(0.001f)] public float FrameRate = 30f;
     [Tooltip("Time in seconds to offset the current frame by.")] public float TimeOffset;
     [Tooltip("Specifies whether or not this clip should use the total movie time or the clip movie time for calculating the current frame.\nIn other words, if this is set to false, then the clip would always start with the first frame of the video.")]
     public bool UseGlobalTime;
+    [Range(0f, 1f)] public float BorderSize = 1f;
 
     private LightShowController controller;
     private bool initializedPlayerData;
@@ -34,10 +38,13 @@ public class LightShowBehaviour : PlayableBehaviour {
         while (currentFrame < 0) currentFrame += lightShowVideo.Data.Frames.Count;
         if (!initializedPlayerData) {
             controller.SetChromaKeyColor(lightShowVideo.Data.BackgroundRemovalColor);
+            controller.SetUseScreenBlending(UseScreenBlending);
+            controller.SetBorderSize(BorderSize);
             initializedPlayerData = true;
         }
 
         controller.SetColor(Color);
+        controller.SetTransparency(GlobalTransparency);
         controller.SetFrame(lightShowVideo.Data.Frames[currentFrame]);
     }
 
