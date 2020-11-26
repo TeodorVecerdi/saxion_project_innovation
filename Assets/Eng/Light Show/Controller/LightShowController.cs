be68703527e0b65c2f0770caffe821c6cfe37f5c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-11-26 15:07:18 +0200

    Add multiple projection locations on church

--- Assets/Eng/Light Show/Controller/LightShowController.cs
+++ Assets/Eng/Light Show/Controller/LightShowController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class LightShowController : MonoBehaviour {
+    [HideInInspector] public ProjectionType ProjectionType;
     private Material material;
     private static readonly int CurrentFrame = Shader.PropertyToID("_currentFrame");
     private static readonly int ChromaKeyColor = Shader.PropertyToID("_chromaKeyColor");
@@ -9,10 +10,9 @@ public class LightShowController : MonoBehaviour {
     private static readonly int GlobalTransparency = Shader.PropertyToID("_globalTransparency");
     private static readonly int UseScreenBlending = Shader.PropertyToID("_useScreenBlending");
     private static readonly int BorderSize = Shader.PropertyToID("_borderSize");
-
-    public void SetFrame(Texture2D frame) {
-        material.SetTexture(CurrentFrame, frame);
-    }
+    private static readonly int Offset = Shader.PropertyToID("_offset");
+    private static readonly int Tiling = Shader.PropertyToID("_tiling");
+    private static readonly int BaseColor = Shader.PropertyToID("_baseColor");
 
     public void CloneMaterial() {
         var meshRenderer = GetComponent<MeshRenderer>();
@@ -20,7 +20,11 @@ public class LightShowController : MonoBehaviour {
         material = meshRenderer.material = newMaterial;
     }
 
-    public void SetColor(Color color) {
+    public void SetFrame(Texture2D frame) {
+        material.SetTexture(CurrentFrame, frame);
+    }
+
+    public void SetEmissionColor(Color color) {
         material.SetColor(EmissionColor, color);
     }
 
@@ -38,5 +42,17 @@ public class LightShowController : MonoBehaviour {
 
     public void SetUseScreenBlending(bool useScreenBlending) {
         material.SetFloat(UseScreenBlending, useScreenBlending ? 1.0f : 0.0f);
+    }
+
+    public void SetOffset(Vector2 offset) {
+        material.SetVector(Offset, offset.V4());
+    }
+
+    public void SetTiling(Vector2 tiling) {
+        material.SetVector(Tiling, tiling.V4());
+    }
+
+    public void SetBaseColor(Color baseColor) {
+        material.SetColor(BaseColor, baseColor);
     }
 }
