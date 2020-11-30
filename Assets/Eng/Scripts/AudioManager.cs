bda2c89920049d06dd5f689c0d9ce97dbbaaa77e
Vlad Ilie Miu <miu.vlad00@gmail.com>
2020-11-30 15:41:26 +0200

    fixed sounds to play OnEnable/Disable && added fist tree

--- Assets/Eng/Scripts/AudioManager.cs
+++ Assets/Eng/Scripts/AudioManager.cs
@@ -7,54 +7,33 @@ using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
-    public static AudioManager instance;
+    public Sound sound;
 
     void Awake()
     {
-        //Singleton
-        if (instance == null)
-            instance = this;
-        else
-        {
-            Destroy((gameObject));
-            return;
-        }
-        
-        foreach (Sound s in sounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = Random.Range(s.pitchMin, s.pitchMax);
-            s.source.loop = s.loop;
-        }
-        PlayRandomSound();
+        sound.source = gameObject.AddComponent<AudioSource>(); 
+        sound.source.clip = sound.clip;
+        sound.source.volume = sound.volume;
+        //sound.source.pitch = Random.Range(sound.pitchMin, sound.pitchMax);
+        sound.source.playOnAwake = false;
+        sound.source.loop = sound.loop;
     }
 
-    public void Play(string name)
+    private void Start()
     {
-        Sound sound = Array.Find(sounds, sound => sound.name == name);
-        if (sound == null)
-        {
-            Debug.LogWarning("Sound: " + name + " could not be found!");
-            return;
-        }
-        sound.source.Play();
-        Debug.Log("Playing " + name);
-    }
-    
-    public void PlayRandomSound()
-    {
-        var randomSoundIndex = Random.Range(0, sounds.Length);
-        Sound randomSound = sounds[randomSoundIndex];
-        if (randomSound == null)
-        {
-            Debug.LogWarning("Sound: " + name + " could not be found!");
-            return;
-        }
-        randomSound.source.Play();
-        Debug.Log("Playing " + randomSound.name);
+        sound.source.Stop(); //starts when pressing play for some reason
     }
 
+    private void OnEnable(){
+        if (!sound.source.isPlaying)
+        {
+            sound.source.pitch = Random.Range(sound.pitchMin, sound.pitchMax);
+            sound.source.Play();
+        }
+    }
+
+    private void OnDisable() {
+     if(sound.source.isPlaying)
+         sound.source.Stop();
+    }
 }
